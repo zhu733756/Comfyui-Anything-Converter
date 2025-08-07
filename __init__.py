d8c10fe86b2ffbb424cf17bad31ff31142c2f071
zhu733756 <qq1079333812@gmail.com>
2025-08-07 09:05:13 +0800

    fix
    Signed-off-by: zhu733756 <qq1079333812@gmail.com>

--- __init__.py
+++ __init__.py
@@ -1,15 +1,17 @@
-from .FileConverter import JsonCombiner, LineConverter
+from .FileConverter import JsonCombiner, LineConverter, FileDictConverter
 
 
 # ---------- 节点映射 ----------
 NODE_CLASS_MAPPINGS = {
     "FileConverter.JsonCombiner": JsonCombiner,
     "FileConverter.LineConverter": LineConverter,
+    "FileConverter.FileDictConverter": FileDictConverter,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "FileConverter.JsonCombiner": "Json Combiner",
     "FileConverter.LineConverter": "Line Converter",
+    "FileConverter.FileDictConverter": "File Dict Converter",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
